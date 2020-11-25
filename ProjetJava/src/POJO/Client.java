f24948b165db3afb2e54e9936a286f5fd41e5d65
Pierr1989 <73483843+Pierr1989@users.noreply.github.com>
2020-11-25 12:23:11 +0100

    Ajout POJO
    -client
    -gestionnaire
    -organisateur

---
+++ ProjetJava/src/POJO/Client.java
@@ -0,0 +1,81 @@
+package POJO;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import DAO.BosquetConnection;
+import DAO.ClientDAO;
+
+public class Client extends Personne {
+	
+	private static final long serialVersionUID = 1L;
+	private List<Commande> listeCommande;
+	ClientDAO DAO = new ClientDAO(BosquetConnection.getInstance());
+			
+	/*CONSTRUCTEURS*/
+	public Client() {
+		
+	}
+	
+	
+	public Client(String nom, String prenom, String adresse, String telephone, String pseudo, String password) {
+		super(nom, prenom, adresse, telephone, pseudo, password);
+		listeCommande = new LinkedList<Commande>();
+	}
+
+	public boolean add(Client cli) {
+		return DAO.create(cli);	
+	}
+	
+
+	
+	public Client login(String telephone, String password) {
+	        List<Client> liste = new LinkedList<Client>();
+	        Client cli = new Client();
+
+	        liste = DAO.getAll();
+	        int found = 0;
+	        
+	        for(int i=0; i< liste.size(); i++) {
+	        	if(liste.get(i).getTelephone().equals(telephone)  && liste.get(i).getPassword().equals(password)) {
+	                cli = liste.get(i);
+	                found = 1;
+	        	}
+	        }
+	        
+	        if(found == 0) {
+	        	return null;
+	        }
+	        
+	        return cli;
+
+	}
+
+	public void delete() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Client find(int id) {
+		return DAO.find(id);	
+	}
+	
+	public List<Client> getAll() {
+        return DAO.getAll();
+    }
+
+	
+	//Accesseurs
+	public List<Commande> getCommande() {
+        return listeCommande;
+    }
+    public void setCommande(List<Commande> listeCommande) {
+        this.listeCommande = listeCommande;
+    }
+	
+}
---
+++ ProjetJava/src/POJO/Organisateur.java
@@ -0,0 +1,75 @@
+package POJO;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import DAO.BosquetConnection;
+import DAO.OrganisateurDAO;
+
+public class Organisateur extends Personne {
+
+	private static final long serialVersionUID = 1L;
+	private List<Reservation> listeReservation;
+	/*CONSTRUCTEURS*/
+	public Organisateur() {
+		
+	}
+	
+	public Organisateur(int id, String nom, String prenom, String adresse, String telephone, String pseudo, String password) {
+		super(id, nom, prenom, adresse, telephone, pseudo, password);
+		listeReservation = new LinkedList<Reservation>();
+	}
+	
+	public Organisateur(String nom, String prenom, String adresse, String telephone, String pseudo, String password) {
+		super(nom, prenom, adresse, telephone, pseudo, password);
+		listeReservation = new LinkedList<Reservation>();
+	}
+	
+	//Accesseurs
+	public List<Reservation> getListeReservation() {
+        return listeReservation;
+    }
+    public void setListeReservation(List<Reservation> listeReservation) {
+        this.listeReservation = listeReservation;
+    }
+
+	public boolean add(Organisateur org) {
+		OrganisateurDAO DAO = new OrganisateurDAO(BosquetConnection.getInstance());
+		return DAO.create(org);
+	}
+	
+
+	
+	public Organisateur login(String telephone, String password) {
+		OrganisateurDAO DAO = new OrganisateurDAO(BosquetConnection.getInstance());
+        List<Organisateur> liste = new LinkedList<Organisateur>();
+        Organisateur orga = new Organisateur();
+
+        liste = DAO.getAll();
+        int found = 0;
+        
+        for(int i=0; i< liste.size(); i++) {
+        	if(liste.get(i).getTelephone().equals(telephone)  && liste.get(i).getPassword().equals(password)) {
+                orga = liste.get(i);
+                found = 1;
+        	}
+        }
+        
+        if(found == 0) {
+        	return null;
+        }
+        
+        return orga;
+	}
+
+	public void delete() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
