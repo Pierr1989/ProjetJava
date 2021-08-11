28e4ea22b333d2cc7d6797dd84b0014ad77bd0cc
Pierr1989 <73483843+Pierr1989@users.noreply.github.com>
2021-08-11 10:06:39 +0200

    Modif de reservationDAO et spectacleDAO

--- ProjetJava/src/DAO/ReservationDAO.java
+++ ProjetJava/src/DAO/ReservationDAO.java
@@ -7,6 +7,7 @@ import java.sql.SQLException;
 import java.util.LinkedList;
 import java.util.List;
 
+import POJO.Organisateur;
 import POJO.Reservation;
 
 public class ReservationDAO extends DAO<Reservation> {
@@ -25,7 +26,7 @@ public class ReservationDAO extends DAO<Reservation> {
             state.setDouble(2, obj.getSolde());
             state.setString(3, obj.getStatut());
             state.setDouble(4, obj.getPrix());
-            state.setInt(5, obj.getIdOrganisateur());
+            state.setInt(5, obj.getOrganisateur().getId());
             state.execute();
             ResultSet rs = state.getGeneratedKeys();
             if(rs.next()) {
@@ -45,8 +46,20 @@ public class ReservationDAO extends DAO<Reservation> {
 
 	@Override
 	public boolean delete(Reservation obj) {
-		// TODO Auto-generated method stub
-		return false;
+		try
+        {
+            PreparedStatement state = connect.prepareStatement("DELETE FROM Reservation WHERE idReservation = ?");
+            state.setInt(1, obj.getIdReservation());
+            state.execute();
+            return true;
+        }
+
+        catch(SQLException e)
+        {
+            e.printStackTrace();
+        }
+
+        return false;
 	}
 
 	@Override
@@ -57,24 +70,42 @@ public class ReservationDAO extends DAO<Reservation> {
 
 	@Override
 	public Reservation find(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		Reservation reservation = null;
+		Organisateur orga = null;
+		OrganisateurDAO DAO = new OrganisateurDAO(BosquetConnection.getInstance());
+        try{
+        	String sql ="SELECT * FROM Reservation WHERE idReservation = " + id;
+            ResultSet result = this.connect.createStatement(
+                    ResultSet.TYPE_SCROLL_INSENSITIVE,
+                    ResultSet.CONCUR_READ_ONLY).executeQuery(sql);
+            if(result.first()) {
+            	orga = DAO.find(result.getInt("res_orga_k"));
+            	reservation = new Reservation(id, orga, result.getDouble("acompte"), result.getDouble("solde"), result.getString("statut"), result.getDouble("prix"));
+            }
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+        }
+        return reservation;
 	}
 
 	@Override
     public List<Reservation> getAll() {
         List<Reservation> list = new LinkedList<Reservation>();
+        Organisateur orga = new Organisateur();
+        OrganisateurDAO DAO = new OrganisateurDAO(BosquetConnection.getInstance());
          try {
              String sql ="SELECT * FROM Reservation";
              ResultSet result = this.connect.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY).executeQuery(sql);
              while(result.next()) {
                  Reservation res = new Reservation();
+                 orga = DAO.find(result.getInt("res_orga_k"));
                  res.setIdReservation(result.getInt("idReservation"));
                  res.setAcompte(result.getDouble("acompte"));
                  res.setSolde(result.getDouble("solde"));
                  res.setStatut(result.getString("statut"));
                  res.setPrix(result.getDouble("prix"));
-                 res.setIdOrganisateur(result.getInt("res_orga_k"));
+                 res.setOrganisateur(orga);
                  list.add(res);
             }
          } catch (SQLException e) {
--- ProjetJava/src/DAO/SpectacleDAO.java
+++ ProjetJava/src/DAO/SpectacleDAO.java
@@ -7,12 +7,11 @@ import java.sql.SQLException;
 import java.util.LinkedList;
 import java.util.List;
 
-import POJO.Client;
+import POJO.Configuration;
 import POJO.PlanningSalle;
 import POJO.Spectacle;
 
-public class SpectacleDAO extends DAO<Spectacle> {
-	private int idRecup = 0;	
+public class SpectacleDAO extends DAO<Spectacle> {	
 	
 	public SpectacleDAO(Connection conn){
         super(conn);
@@ -23,12 +22,13 @@ public class SpectacleDAO extends DAO<Spectacle> {
 		int id = 0;
 		try
         {			
-			
-            PreparedStatement state = connect.prepareStatement("INSERT INTO Spectacle(idSpectacle, titre, nbrePlaceParCLient, spec_plan_k) VALUES (?,?,?,?)");
+            PreparedStatement state = connect.prepareStatement("INSERT INTO Spectacle(idSpectacle, titre, description, nbrePlaceParCLient, spec_plan_k, spec_conf_k) VALUES (?,?,?,?,?,?)");
             state.setInt(1, obj.getIdSpectacle());
-            state.setString(2, obj.getTitre());                      
-            state.setInt(3, obj.getNbrPlaceParClient());
-            state.setInt(4,  obj.getIdPlanningSalle()); 
+            state.setString(2, obj.getTitre());   
+            state.setString(3, obj.getDescription()); 
+            state.setInt(4, obj.getNbrPlaceParClient());
+            state.setInt(5,  obj.getPlanning().getIdPlanning()); 
+            state.setInt(6,  obj.getConf().getIdConfiguration());
             state.execute();
             ResultSet rs = state.getGeneratedKeys();
             if(rs.next()) {
@@ -37,12 +37,10 @@ public class SpectacleDAO extends DAO<Spectacle> {
             }	              
             return true;
         }
-
         catch(SQLException e)
         {
             e.printStackTrace();
         }
-        
         return false;
 	}
 
@@ -51,7 +49,6 @@ public class SpectacleDAO extends DAO<Spectacle> {
 		try
         {
             PreparedStatement state = connect.prepareStatement("DELETE FROM Spectacle WHERE idSpectacle = ?");
-			//PreparedStatement state = connect.prepareStatement("CASCADE ON DELETE FROM Spectacle INNER JOIN Artiste ON Spectacle.idSpectacle = Artiste.arti_spe_k INNER JOIN Configuration ON Spectacle.idSpectacle = Configuration.conf_spec_k INNER JOIN Categorie ON Configuration.idConfiguration = Categorie.cat_conf_k WHERE idSpectacle = ?");
             state.setInt(1, obj.getIdSpectacle());
             state.execute();
             return true;
@@ -73,21 +70,48 @@ public class SpectacleDAO extends DAO<Spectacle> {
 
 	@Override
 	public Spectacle find(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		Spectacle spectacle = null;
+		PlanningSalle plan = null;
+		Configuration conf = null;
+		PlanningSalleDAO planDAO = new PlanningSalleDAO(BosquetConnection.getInstance());
+		ConfigurationDAO confDAO = new ConfigurationDAO(BosquetConnection.getInstance());
+        try{
+        	String sql ="SELECT * FROM Spectacle WHERE idSpectacle = " + id;
+            ResultSet result = this.connect.createStatement(
+                    ResultSet.TYPE_SCROLL_INSENSITIVE,
+                    ResultSet.CONCUR_READ_ONLY).executeQuery(sql);
+            if(result.first()) {
+            	plan = planDAO.find(result.getInt("spec_plan_k"));
+            	conf = confDAO.find(result.getInt("spec_conf_k"));
+            	spectacle = new Spectacle(id, plan, result.getString("titre"), result.getString("description"), result.getInt("nbrePlaceParClient"), conf);
+            }
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+        }
+        return spectacle;
 	}
 
 	@Override
     public List<Spectacle> getAll() {
         List<Spectacle> list = new LinkedList<Spectacle>();
+        PlanningSalle plann = new PlanningSalle();
+        Configuration conf = null;
+        PlanningSalleDAO DAO = new PlanningSalleDAO(BosquetConnection.getInstance());
+        ConfigurationDAO confDAO = new ConfigurationDAO(BosquetConnection.getInstance());
          try {
              String sql ="SELECT * FROM Spectacle";
              ResultSet result = this.connect.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY).executeQuery(sql);
              while(result.next()) {
-            	 Spectacle spectacle = new Spectacle();
+            	 Spectacle spectacle = new Spectacle();           	 
+            	 plann = DAO.find(result.getInt("spec_plan_k"));
+            	 conf = confDAO.find(result.getInt("spec_conf_k"));
             	 spectacle.setIdSpectacle(result.getInt("idSpectacle"));
             	 spectacle.setTitre(result.getString("titre"));
+            	 spectacle.setDescription(result.getString("description"));
             	 spectacle.setNbrPlaceParClient(result.getInt("nbrePlaceParClient"));
+            	 spectacle.setPlanning(plann);
+            	 spectacle.setConf(conf);
                  list.add(spectacle);
             }
          } catch (SQLException e) {
