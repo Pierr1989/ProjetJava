fe99ebaacb1bfe4407cf6ed4bb10c1da3534714e
Pierr1989 <73483843+Pierr1989@users.noreply.github.com>
2021-08-12 08:39:59 +0200

    Derniers ajouts de frames
    ResumeCommande et SelectionTicket ajoutés.

---
+++ ProjetJava/src/Window/ResumeCommandeClient.java
@@ -0,0 +1,42 @@
+package Window;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class ResumeCommandeClient extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ResumeCommandeClient frame = new ResumeCommandeClient();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public ResumeCommandeClient() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+}
